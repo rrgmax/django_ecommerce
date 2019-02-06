d6a702b28f29d908438b73d011fdce2e5ea5433b
Rogerio <rrgmax@bol.com.br>
2019-02-06 03:48:24 -0200

    Added import Http404 into products/views.py and setted the FBV

--- products/views.py
+++ products/views.py
@@ -1,17 +1,17 @@
+from django.http import Http404
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
 #Class Based View
-
 class ProductListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
     template_name = "products/list.html"
-
+    
     #def get_context_data(self, *args, **kwargs):
-        #context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        #context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         #print(context)
         #return context
 
@@ -29,7 +29,7 @@ class ProductDetailView(DetailView):
     queryset = Product.objects.all()
     template_name = "products/detail.html"
     
-    def get_context_data(self, *args, **kwargs):
+    def get_context_data(self):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
         return context
@@ -37,7 +37,13 @@ class ProductDetailView(DetailView):
 #Function Based View
 def product_detail_view(request, pk = None, *args, **kwargs):
     #instance = Product.objects.get(pk = pk) #get the object id
-    instance = get_object_or_404(Product, pk = pk)
+    #instance = get_object_or_404(Product, pk = pk)
+    qs = Product.objects.filter(id = pk)
+    if qs.count() == 1:
+        instance = qs.first()
+    else:
+        raise Http404("Esse produto não existe!")
+
     context = {
         'object': instance
     }
